f11de600f90d5f7826137529758aa6ffafee6698
Arman Jindal <arman.jindal@columbia.edu>
2020-10-24 10:49:27 +0530

    Wrote custom action to get friend name input into new message

--- ecce-rasa-2.0/actions/actions.py
+++ ecce-rasa-2.0/actions/actions.py
@@ -1,6 +1,7 @@
 from typing import Text, List, Any, Dict
 
-from rasa_sdk import Tracker, FormValidationAction
+
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
@@ -38,20 +39,28 @@ class ValidateFirstTimeForm(FormValidationAction):
             # validation succeeded, set the value of the "cuisine" slot to value
             return {"age": slot_value}
         else:
-            dispatcher.utter_message(template="utter_wrong_format", err="You can't be less than 0!")
+            dispatcher.utter_message(template="utter_wrong_format", err="You can't be less than 0 :) !")
             return {"age": None}
     
 
 
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class FractionsHalvesIntroduction2(Action):
+
+    def name(self) -> Text:
+        return "action_fractions_halves_introduction_2"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        friend_name = next(tracker.get_latest_entity_values("name"), None)
+        prev_name = tracker.get_slot(key="friend_1")
+        if friend_name:
+            print(f"THIS RAN. Name:{friend_name} Prev_Name: {prev_name}")
+            dispatcher.utter_message(template="utter_fractions_halves_introduction_2", friend_1=friend_name)
+            return [SlotSet(key="friend_1", value=friend_name)]
+        else:
+            dispatcher.utter_message(text=f"I did not quite get the name. Lets call your friend {prev_name}")
+            dispatcher.utter_message(template="utter_fractions_halves_introduction_2")
+
+        return []
