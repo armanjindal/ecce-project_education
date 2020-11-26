53a451a8487f2852b7342be3cf96b6ea9b7245ce
Arman Jindal <arman.jindal@columbia.edu>
2020-11-26 12:03:52 +0530

    Start backend building for question bank

--- actions/actions.py
+++ actions/actions.py
@@ -11,44 +11,51 @@ from fuzzywuzzy import process
 
 def extractFraction(tracker):
     # Error handle exceptions 
-    return tracker.latest_message.get('entities')[0].get('text').lower()
-
-def mcq_match(user_input, question, key , method="FuzzyMatch", cutoff=60):
-    # Method used to match a single word 
+    fraction = None
+    try:
+        fraction = tracker.latest_message.get('entities')[0].get('text').lower()
+    except Error as e:
+        print(e)
+    return fraction
+    
+def check_question(user_input, question, cutoff=60):
+    # TODO: Turn this into a external database. Query API here to get q_list
     mcq_options_dict = {
-    "action_lesson_options": ["fractions", "multiplication"],
-    "validate_fractions_halves_story_form": {
-        "fractions_halves_mcq_1": [['a','c','d','e'], 'c']
-        },
-    "validate_fractions_parts_story_form": {
-        "object_2": ["badams", "biscuits", "grapes", "chocolates"],
-        "fractions_parts_mcq_2" : ["more", "I get more", "greater"] 
-        }
+    "fractions_halves_mcq_1": ["mcq_match", ['a','c','d','e'], 'c'],
+    "object_2": ["option_match", ["badams", "biscuits", "grapes", "chocolates"]],
+    "fractions_parts_mcq_2" : ["frq_keyword_match", ["more", "I get more", "greater"], ["less", "fewer"]]
     }
-    # Fuzzy match strings and returns closest match result
-    if method=="FuzzyMatch":
-        resp = user_input.lower()
-        options = mcq_options_dict[f"{question}"]
-        if key:
-            options = options[f"{key}"]
+    q_list = mcq_options_dict[question]
+    question_type = q_list[0]
+    resp = user_input.lower()
+    # Fuzzy match strings and returns closest match result to list of options
+    if question_type=="option_match":
+        options = q_list[1]
         result = process.extractOne(resp, options, score_cutoff=cutoff)
         if result:
             return result[0]
         else:
             return None 
-    
     # Returns exact match to options i.e 'A, B, C, D' 
-    if method=="DirectMatch":
-        resp = user_input.lower()
-        answer_list = mcq_options_dict[f"{question}"][f"{key}"]
-        if resp in answer_list[0]:
-            if resp == answer_list[1]:
+    if question_type=="mcq_match":
+        options = q_list[1]
+        if resp in options:
+            if resp == q_list[2]:
                 return ["CORRECT", resp]
             else:
                 return ["INCORRECT", resp]
         else:
             return None
-
+    # Checks free text input if it contains list 1 and does not contain words for list 2
+    if question_type == "keyword_match":
+        resp_words = resp.split()
+        keywords, not_words = q_list[0] , q_list[1]
+        if any(word in keywords for word in resp_words):
+            if any(word in not_words for word in resp_words):
+                return "AMBIG" # Included words in both lists
+            return "CORRECT"
+        else:
+            return "INCORRECT"
 
 class ValidateFirstTimeForm(FormValidationAction):
     def name(self) -> Text:
@@ -80,7 +87,8 @@ class ValidateFirstTimeForm(FormValidationAction):
         else:
             dispatcher.utter_message(template="utter_wrong_format", err="You can't be less than 0  :) !")
             return {"age": None}
-    
+
+# TODO: Find a better way to get user input mid lesson
 class FractionsHalvesIntroduction2(Action):
     def name(self) -> Text:
         return "action_fractions_halves_introduction_2"
@@ -92,7 +100,7 @@ class FractionsHalvesIntroduction2(Action):
         friend_name = next(tracker.get_latest_entity_values("name"), None)
         prev_name = tracker.get_slot(key="friend_1")
         if friend_name:
-            dispatcher.utter_message(template="utter_fractions_halves_introduction_2", friend_1=friend_name)
+            dispatcher.utter_message(template="utter_fractions_halves_introduction_2", friend_1=friend_name.capitalize())
             return [SlotSet(key="friend_1", value=friend_name.capitalize())]
         else:
             dispatcher.utter_message(text=f"I did not quite get the name. Lets call just call your friend {prev_name}!s")
@@ -111,7 +119,7 @@ class ValidateFractionHalvesStoryForm(FormValidationAction):
         domain: DomainDict,
     ) -> Dict[Text, Any]:
         slot_name = "fractions_halves_mcq_1"
-        answer = mcq_match(slot_value, self.name(), slot_name, "DirectMatch") 
+        answer = check_question(slot_value, slot_name) 
         if answer:
             if answer[0] == "CORRECT":
                 dispatcher.utter_message(template="utter_correct")
@@ -155,8 +163,7 @@ class ValidateFractionPartsStoryForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        
-        match = mcq_match(slot_value, self.name(), "object_2")
+        match = check_question(slot_value, "object_2")
         if match:
             return{"object_2": match}
         else:
@@ -209,20 +216,34 @@ class ValidateFractionPartsStoryForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        slot_name = "fractions_parts_mcq_2"
-        result = mcq_match(slot_value, self.name(), slot_name)
-        if result and slot_value.lower() not in ["lower", "less"]:
+        result = check_question(slot_value, "fractions_parts_mcq_2")
+        if result:
             dispatcher.utter_message(template="utter_correct")
         else:
             dispatcher.utter_message(template= "utter_incorrect")
-        dispatcher.utter_message(template="utter_fractions_parts_mcq_2_explanation")
         
-        return{"fractions_parts_mcq_2": result}
+        dispatcher.utter_message(template="utter_fractions_parts_mcq_2_explanation")
+        return{"fractions_parts_mcq_2": slot_value}
 
-
-""" Temporay helper methods that will be defined correctly 
-once basic functionality is established """
-
-
-
-# # TODO: Create Q/A database 
+class ValidateFractionWholesStoryForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_fractions_parts_wholes_form"
+    
+    def validate_fractions_wholes_nrq_1(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:        
+        if slot_value and int(slot_value) > 0:
+            if int(slot_value) == 4:
+                dispatcher.utter_message(template="utter_correct")
+            else:
+                dispatcher.utter_message(template="utter_incorrect")
+            
+            dispatcher.utter_message(template="utter_fractions_wholes_nrq_1_explanation")
+            return {"fractions_parts_wholes_1": slot_value}
+        else:
+            dispatcher.utter_message(template="utter_wrong_format", err="Give me a number!")
+            return {"fractions_parts_nrq_1": None}
