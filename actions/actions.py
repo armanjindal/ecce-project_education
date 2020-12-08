c816ba15511ea546592778d971eeac59a884286c
Arman Jindal <arman.jindal@columbia.edu>
2020-12-08 13:19:11 +0530

    Refactor action server code, implement lessons to wholes

--- actions/actions.py
+++ actions/actions.py
@@ -8,55 +8,120 @@ from rasa_sdk.types import DomainDict
 
 #import helpers
 from fuzzywuzzy import process
+# TODO: Figure out why I cannot import local .py files
 
 def extractFraction(tracker):
     # Error handle exceptions 
+    print(tracker)
     fraction = None
     try:
         fraction = tracker.latest_message.get('entities')[0].get('text').lower()
-    except Error as e:
-        print(e)
+    except:
+        print("AN ERROR in EXTRACT FRACTION OCCURED")
+        return None
     return fraction
-    
-def check_question(user_input, question, cutoff=60):
-    # TODO: Turn this into a external database. Query API here to get q_list
-    mcq_options_dict = {
-    "fractions_halves_mcq_1": ["mcq_match", ['a','c','d','e'], 'c'],
-    "object_2": ["option_match", ["badams", "biscuits", "grapes", "chocolates"]],
-    "fractions_parts_mcq_2" : ["frq_keyword_match", ["more", "I get more", "greater"], ["less", "fewer"]]
-    }
-    q_list = mcq_options_dict[question]
-    question_type = q_list[0]
+
+def matchOption(user_input, slot_name, cutoff=60):
+    # TODO: Turn this into an external database
     resp = user_input.lower()
+    options_dict = {
+    "object_2": ["option_match", ["badams", "biscuits", "grapes", "chocolates"]]
+    }
+    option_matching_method = options_dict[slot_name][0]
     # Fuzzy match strings and returns closest match result to list of options
-    if question_type=="option_match":
-        options = q_list[1]
+    if option_matching_method =="option_match":
+        options = options_dict[slot_name][1]
         result = process.extractOne(resp, options, score_cutoff=cutoff)
         if result:
             return result[0]
         else:
             return None 
+
+def checkQuestion(user_input, question, tracker=None):
+    """ Takes in the expected user input, queries DB, 
+    and validates the user input against the question type"""
+
+    # TODO: Turn this into a external database. Turn dict into DB query
+    questions_dict = {
+    "fractions_halves_mcq_1": ["mcq_match", ['a','c','d','e'], 'c'],
+    "fractions_halves_frq_1": ["frq_keyword_match", ["equal", "halves", "half", "same", "identical"], None],
+    "fractions_parts_mcq_1" : ["nrq_fractions", ["1/3", "2/6"]],
+    "fractions_parts_mcq_2": ["frq_keyword_match", ["more", "greater"], ["less", "fewer"]],
+    "fractions_parts_nrq_1": ["nrq_numeral", [2]],
+    "fractions_parts_mcq_3": ["frq_keyword_match", ["more", "greater"], ["less", "fewer"]],
+    "fractions_parts_mcq_4": ["frq_keyword_match", ["more", "greater"], ["less", "fewer"]],
+    "fractions_wholes_nrq_1": ["nrq_numeral", [4]],
+    "fractions_wholes_nrq_2": ["nrq_fractions", ["1/3", "2/6"]],
+    "fractions_wholes_frq_1": ["frq_keyword_match", ["box", "4"], None],
+    "fractions_wholes_nrq_3": ["nrq_numeral", [1]],
+    "fractions_wholes_nrq_4": ["nrq_fractions", ["1/4"]],
+    "fractions_wholes_nrq_5": ["nrq_fractions", ["1/6"]],
+    }
+    q_list = questions_dict[question]
+    question_type = q_list[0]
+    
+    if question_type == "nrq_fractions":
+        if user_input > 0 and user_input <= 1:
+            text_fraction = extractFraction(tracker)
+            if text_fraction:
+                if text_fraction in q_list[1]:
+                    return "CORRECT"
+                else:
+                    return "INCORRECT"
+        return "WRONG_FORMAT"
+    
+    if question_type == "nrq_numeral":
+        if user_input:
+            for ans_option in q_list[1]:
+                if user_input == ans_option:
+                    return "CORRECT"
+            return "INCORRECT"
+        else:
+            return "WRONG_FORMAT"
     # Returns exact match to options i.e 'A, B, C, D' 
     if question_type=="mcq_match":
+        resp = user_input.lower()
         options = q_list[1]
         if resp in options:
             if resp == q_list[2]:
-                return ["CORRECT", resp]
+                return "CORRECT"
             else:
-                return ["INCORRECT", resp]
+                return "INCORRECT"
         else:
             return None
     # Checks free text input if it contains list 1 and does not contain words for list 2
-    if question_type == "keyword_match":
+    if question_type == "frq_keyword_match":
+        resp = user_input.lower()
         resp_words = resp.split()
-        keywords, not_words = q_list[0] , q_list[1]
+        keywords = q_list[1] 
         if any(word in keywords for word in resp_words):
-            if any(word in not_words for word in resp_words):
-                return "AMBIG" # Included words in both lists
+            not_words = q_list[2]
+            if not_words:
+                if any(word in not_words for word in resp_words):
+                    return "AMBIG" # Included words in both lists
             return "CORRECT"
         else:
             return "INCORRECT"
 
+def respondQuestion(answer, question, slot_value, dispatcher):
+    slot_dict_input = None
+    if answer == "CORRECT":
+        dispatcher.utter_message(template="utter_correct")
+        slot_dict_input = slot_value
+    if answer == "INCORRECT":
+        dispatcher.utter_message(template="utter_incorrect")
+        # TODO: Add hint counter / progression HERE
+        slot_dict_input = slot_value
+    if answer == "WRONG_FORMAT":
+        dispatcher.utter_message(template="utter_wrong_format", err="Give me a number!")
+        slot_dict_input = None
+    if answer == "AMBIG":
+        dispatcher.utter_message(template="utter_wrong_format", err="That is a confusing answer!")
+        slot_dict_input = None
+    if slot_dict_input:
+        dispatcher.utter_message(template=f"utter_{question}_explanation")
+    return slot_dict_input
+    
 class ValidateFirstTimeForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_first_time_form"
@@ -72,7 +137,7 @@ class ValidateFirstTimeForm(FormValidationAction):
             name = str(slot_value)
             return {"user_name": name.capitalize()}
         else:
-            dispatcher.utter_message(text="I can't seem to recognize your name!")
+            dispatcher.utter_message(text="I can't seem to recognize your name, friend!")
             return {"user_name": "friend"}
     
     def validate_age(
@@ -88,28 +153,26 @@ class ValidateFirstTimeForm(FormValidationAction):
             dispatcher.utter_message(template="utter_wrong_format", err="You can't be less than 0  :) !")
             return {"age": None}
 
-# TODO: Find a better way to get user input mid lesson
-class FractionsHalvesIntroduction2(Action):
-    def name(self) -> Text:
-        return "action_fractions_halves_introduction_2"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        friend_name = next(tracker.get_latest_entity_values("name"), None)
-        prev_name = tracker.get_slot(key="friend_1")
-        if friend_name:
-            dispatcher.utter_message(template="utter_fractions_halves_introduction_2", friend_1=friend_name.capitalize())
-            return [SlotSet(key="friend_1", value=friend_name.capitalize())]
-        else:
-            dispatcher.utter_message(text=f"I did not quite get the name. Lets call just call your friend {prev_name}!s")
-            dispatcher.utter_message(template="utter_fractions_halves_introduction_2")
-        return []
-
 class ValidateFractionHalvesStoryForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_fractions_halves_story_form"
+    
+    # TODO: Change once you understand how to deal with a non extracted entity
+    def validate_friend_1(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        defualt_name = "Puja"
+        if slot_value and len(slot_value) > 0:
+            dispatcher.utter_message(template=f"utter_friend_1_explanation", friend_1=slot_value)
+            return {"friend_1": slot_value}
+        else:
+            dispatcher.utter_message(f"I can't find the name! Lets call your friend {default_name} for now!")
+            dispatcher.utter_message(template=f"utter_friend_1_explanation", friend_1=default_name)           
+            return {"friend_1": defualt_name}
 
     def validate_fractions_halves_mcq_1(
         self,
@@ -118,19 +181,10 @@ class ValidateFractionHalvesStoryForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        slot_name = "fractions_halves_mcq_1"
-        answer = check_question(slot_value, slot_name) 
-        if answer:
-            if answer[0] == "CORRECT":
-                dispatcher.utter_message(template="utter_correct")
-                return {"fractions_halves_mcq_1": answer[1]}
-            else:
-                dispatcher.utter_message(template="utter_incorrect")
-                dispatcher.utter_message(text= "Let try it again!")
-                return {"fractions_halves_mcq_1": None}
-        else:
-            dispatcher.utter_message(template="utter_wrong_format", err="I need a one letter answer like 'A' or 'D' ")
-            return {"fractions_halves_mcq_1": None}
+        question_name = "fractions_halves_mcq_1"
+        answer = checkQuestion(slot_value, question_name)
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name: slot_dict_input}
 
     def validate_fractions_halves_frq_1(
         self,
@@ -139,18 +193,10 @@ class ValidateFractionHalvesStoryForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        answer_keywords = ["equal", "halves", "half", "same", "identical"]
-        response = slot_value.lower()
-        if response:
-            if any(word in answer_keywords for word in response.split()):
-                dispatcher.utter_message(template="utter_correct")
-            else:
-                dispatcher.utter_message(template="utter_incorrect")
-            dispatcher.utter_message(template="utter_fractions_halves_frq_1_explanation")
-            return{"fractions_halves_frq_1": response}
-        else:
-            dispatcher.utter_message(template= "utter_wrong_format", err= "I need a longer answer than that !")
-            return{"fractions_halves_frq_1": None}
+        question_name = "fractions_halves_frq_1"
+        answer = checkQuestion(slot_value, question_name)
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name: slot_dict_input}
 
 class ValidateFractionPartsStoryForm(FormValidationAction):
     def name(self) -> Text:
@@ -163,7 +209,7 @@ class ValidateFractionPartsStoryForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        match = check_question(slot_value, "object_2")
+        match = matchOption(slot_value, "object_2")
         if match:
             return{"object_2": match}
         else:
@@ -177,17 +223,10 @@ class ValidateFractionPartsStoryForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:        
-        if slot_value and int(slot_value) > 0:
-            if int(slot_value) == 2:
-                dispatcher.utter_message(template="utter_correct")
-            else:
-                dispatcher.utter_message(template="utter_incorrect")
-            
-            dispatcher.utter_message(template="utter_fractions_parts_nrq_1_explanation")
-            return {"fractions_parts_nrq_1": slot_value}
-        else:
-            dispatcher.utter_message(template="utter_wrong_format", err="Give me a number!")
-            return {"fractions_parts_nrq_1": None}
+        question_name = "fractions_parts_nrq_1"
+        answer = checkQuestion(slot_value, question_name)
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name: slot_dict_input}
     
     def validate_fractions_parts_mcq_1(
         self,
@@ -196,18 +235,11 @@ class ValidateFractionPartsStoryForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        if slot_value > 0 and slot_value <= 1:
-            text_fraction = extractFraction(tracker)
-            if text_fraction == '1/3':
-                dispatcher.utter_message(template="utter_correct")
-                dispatcher.utter_message(template="utter_fractions_parts_mcq_1_explanation")
-                return {"fractions_parts_mcq_1": text_fraction}
-            else:
-                dispatcher.utter_message(template="utter_fractions_parts_mcq_1_options")
-                return {"fractions_parts_mcq_1": None}
-        else:
-            dispatcher.utter_message(template="utter_wrong_format", err="Give me a fraction! For example 1/2 or 1/4")
-            return {"fractions_parts_mcq_1": None}          
+        question_name = "fractions_parts_mcq_1"
+        answer = checkQuestion(slot_value, question_name, tracker=tracker)
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name: slot_dict_input}
+
 
     def validate_fractions_parts_mcq_2(
         self,
@@ -216,18 +248,38 @@ class ValidateFractionPartsStoryForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        result = check_question(slot_value, "fractions_parts_mcq_2")
-        if result:
-            dispatcher.utter_message(template="utter_correct")
-        else:
-            dispatcher.utter_message(template= "utter_incorrect")
-        
-        dispatcher.utter_message(template="utter_fractions_parts_mcq_2_explanation")
-        return{"fractions_parts_mcq_2": slot_value}
+        question_name =  "fractions_parts_mcq_2"
+        answer = checkQuestion(slot_value, question_name)
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name:slot_dict_input}
+
+    def validate_fractions_parts_mcq_3(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        question_name =  "fractions_parts_mcq_3"
+        answer = checkQuestion(slot_value, question_name)
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name:slot_dict_input}
+    
+    def validate_fractions_parts_mcq_4(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        question_name =  "fractions_parts_mcq_4"
+        answer = checkQuestion(slot_value, question_name)
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name:slot_dict_input}
 
 class ValidateFractionWholesStoryForm(FormValidationAction):
     def name(self) -> Text:
-        return "validate_fractions_parts_wholes_form"
+        return "validate_fractions_wholes_story_form"
     
     def validate_fractions_wholes_nrq_1(
         self,
@@ -236,14 +288,73 @@ class ValidateFractionWholesStoryForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:        
-        if slot_value and int(slot_value) > 0:
-            if int(slot_value) == 4:
-                dispatcher.utter_message(template="utter_correct")
-            else:
-                dispatcher.utter_message(template="utter_incorrect")
-            
-            dispatcher.utter_message(template="utter_fractions_wholes_nrq_1_explanation")
-            return {"fractions_parts_wholes_1": slot_value}
-        else:
-            dispatcher.utter_message(template="utter_wrong_format", err="Give me a number!")
-            return {"fractions_parts_nrq_1": None}
+        question_name = "fractions_wholes_nrq_1"
+        answer = checkQuestion(slot_value, question_name)
+        print(answer)    
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name: slot_dict_input}
+    
+    def validate_fractions_wholes_nrq_2(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:        
+        question_name = "fractions_wholes_nrq_2"
+        answer = checkQuestion(slot_value, question_name, tracker=tracker)
+        print(answer)    
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name: slot_dict_input}
+    
+    def validate_fractions_wholes_frq_1(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:        
+        question_name = "fractions_wholes_frq_1"
+        answer = checkQuestion(slot_value, question_name)
+        print(answer)
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name: slot_dict_input}    
+    
+    def validate_fractions_wholes_nrq_3(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:        
+        question_name = "fractions_wholes_nrq_3"
+        answer = checkQuestion(slot_value, question_name, tracker=tracker)
+        print(answer)
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name: slot_dict_input}
+    
+    def validate_fractions_wholes_nrq_4(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:        
+        question_name = "fractions_wholes_nrq_4"
+        answer = checkQuestion(slot_value, question_name, tracker=tracker)
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        return {question_name: slot_dict_input}
+    
+    def validate_fractions_wholes_nrq_5(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:        
+        question_name = "fractions_wholes_nrq_5"
+        answer = checkQuestion(slot_value, question_name, tracker=tracker)
+        print(answer)
+        slot_dict_input = respondQuestion(answer, question_name, slot_value, dispatcher)
+        print(slot_dict_input)
+        return {question_name: slot_dict_input}
